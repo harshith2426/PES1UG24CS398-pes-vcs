5c2de090167efa002a4e34a6415601680a8c4938
Sai Harshith <saisvk2426@gmail.com>
2026-04-20 18:45:19 +0530

    Phase 4: fill commit metadata

--- commit.c
+++ commit.c
@@ -207,6 +207,10 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
         commit.has_parent = 0;
     }
 
+    snprintf(commit.author, sizeof(commit.author), "%s", pes_author());
+    commit.timestamp = (uint64_t)time(NULL);
+    snprintf(commit.message, sizeof(commit.message), "%s", message);
+
     (void)commit_id_out;
     return -1;
 }
